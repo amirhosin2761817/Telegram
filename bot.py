45da21fed65b1fce8764fa6ae32ae1b894eaf25c
amirhosin2761817 <amirhosingamer575@gmail.com>
2025-08-06 01:58:06 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,28 @@
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-def reply(update, context):
-    if 'میلاد' in update.message.text:
-        update.message.reply_text(
-            'قیمت ساعتی:\n'
-            'تو ماشین ۵۰۰\n'
-            'هتل ۱ تومن\n'
-            'خونه ۲ تومن\n'
-            'قیمت یه شب کامل:\n'
-            'تو ماشین ۳ تومن\n'
-            'هتل ۶ تومن\n'
-            'خونه ۱۰ تومن'
+TOKEN = "8210753392:AAF8Y_lps0OkktfseLPJfSrbbm_AR7xdkgU"
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text.lower()
+    if "میلاد" in text:
+        reply_text = (
+            "قیمت ساعتی:\n"
+            "تو ماشین ۵۰۰\n"
+            "هتل ۱ تومن\n"
+            "خونه ۲ تومن\n\n"
+            "قیمت یه شب کامل:\n"
+            "تو ماشین ۳ تومن\n"
+            "هتل ۶ تومن\n"
+            "خونه ۱۰ تومن"
         )
-
-def main():
-    updater = Updater("8210753392:AAF8Y_lps0OkktfseLPJfSrbbm_AR7xdkgU", use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, reply))
-    updater.start_polling()
-    updater.idle()
+        await update.message.reply_text(reply_text)
 
 if __name__ == '__main__':
-    main()
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message)
+    app.add_handler(message_handler)
+
+    print("ربات داره کار می‌کنه...")
+    app.run_polling()
